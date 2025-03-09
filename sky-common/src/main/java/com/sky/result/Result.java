a0f58ffcb23f5d15f0b12400d37b751a248f1b1e
bbz <499718384@qq.com>
2025-03-09 19:38:08 +0800

    新增员工相关的业务代码

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -21,12 +21,16 @@ public class Result<T> implements Serializable {
         return result;
     }
 
+
+
     public static <T> Result<T> success(T object) {
         Result<T> result = new Result<T>();
         result.data = object;
         result.code = 1;
         return result;
     }
+
+
 
     public static <T> Result<T> error(String msg) {
         Result result = new Result();
